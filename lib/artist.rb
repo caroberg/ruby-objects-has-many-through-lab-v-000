56907a013556c95727d22fee10a7c62936e2dd50
Caroline Berg <carolineeberg@gmail.com>
2018-09-16 17:29:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,10 +5,16 @@ class Artist
   def initialize(name)
     @name = name
     @@all << self
+    @songs = []
   end
 
   def self.all
     @@all
+  end
+
+  def new_song(song)
+    @songs << song 
+    song.artist = self 
   end
 
 end
