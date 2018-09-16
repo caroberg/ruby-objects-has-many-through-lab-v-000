b0ae60ccc6357248570797272eb3737ede707537
Caroline Berg <carolineeberg@gmail.com>
2018-09-16 17:35:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,22 +1,31 @@
 class Artist
-  attr_accessor :name
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-    @songs = []
+  attr_accessor :name, :artist, :genre
+   @@all = []
+   
+   def initialize(name)
+     @name = name
+     @@all << self
+   end
+   
+   def self.all
+     @@all
+   end
+   
+   def new_song(name, genre)
+     song = Song.new(name, self, genre)
+     self.songs.last
+   end
+   
+   def songs
+     Song.all.select do |song|
+     song.artist == self
+    end
+   end
+   
+   def genres
+    Song.all.map do |song|
+      song.genre
+    end
   end
-
-  def self.all
-    @@all
-  end
-
-  def new_song(song)
-    @songs << song
-    song.artist = self
-  end
-
-
 
 end
