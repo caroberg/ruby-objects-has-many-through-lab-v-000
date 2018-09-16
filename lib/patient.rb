0b4bee164e47f9680a2f37e1eb9673814ea39f59
Caroline Berg <carolineeberg@gmail.com>
2018-09-16 18:34:24 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,24 +1,33 @@
-
 class Patient
-  attr_accessor :name
+  attr_accessor :name, :doctor, :date
+  @@all = []
 
   def initialize(name)
     @name = name
     @appointments = []
+    @@all << self
   end
 
-  def add_appointment(appointment)
-    @appointments << appointment
-    appointment.patient = self
+  def self.all
+    @@all
+  end
+
+  def new_appointment(doctor, date)
+    appointment = Appointment.new(doctor, self, date)
   end
 
   def appointments
-    @appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
   end
 
   def doctors
-    @appointments.collect do |appointment|
+    Appointment.all.map do |appointment|
       appointment.doctor
     end
   end
 end
+
+
+
