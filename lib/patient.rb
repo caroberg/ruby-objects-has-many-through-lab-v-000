c2576e55b6aa0a4b4899f609837376becd19c5b3
Caroline Berg <carolineeberg@gmail.com>
2018-09-16 19:45:59 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,7 @@ class Patient
   end
 
   def new_appointment(doctor, date)
-    appointment = Appointment.new(doctor, self, date)
+    appointment = Appointment.new(date, self, doctor)
   end
 
   def appointments
